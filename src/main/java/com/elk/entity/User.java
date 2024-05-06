a31e2895cdc07f15df6d142087ef12d699c60e5c
sam.lee <Sam.Lee@teamp1us.com.tw>
2024-05-06 15:09:47 +0800

    added basic elk setup and example api.

---
+++ src/main/java/com/elk/entity/User.java
@@ -0,0 +1,31 @@
+package com.elk.entity;
+
+import com.elk.entity.embedded.Hobby;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntityBase;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import io.smallrye.common.constraint.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.bson.types.ObjectId;
+
+@EqualsAndHashCode(callSuper = true)
+@MongoEntity(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RegisterForReflection
+@Accessors(chain = true)
+public class User extends ReactivePanacheMongoEntityBase {
+
+    private ObjectId id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private Hobby hobby;
+
+}
